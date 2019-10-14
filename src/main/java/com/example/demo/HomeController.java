3967334b07f817040ddf1a5b35e7a00b26aa5ea7
katcampbell18 <ktrncampbell@gmail.com>
2019-10-14 15:12:49 -0400

    store user info and retrieve current user

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,10 +1,25 @@
 package com.example.demo;
 
+import com.sun.xml.internal.ws.developer.SchemaValidation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.security.Principal;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserRepository userRepository;
+
+
     @RequestMapping("/")
     public String index(){
         return "index";
@@ -18,7 +33,11 @@ public class HomeController {
         return "admin";
     }
     @RequestMapping("/secure")
-    public String secure(){
+    public String secure(Principal principal, Model model){
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
         return "secure";
     }
+
+
 }
